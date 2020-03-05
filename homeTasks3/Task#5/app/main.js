4b200f68d0b6f434ce094bd05718e9ce715e549e
gmaxym <gorbenkomaxym@gmail.com>
2020-03-05 09:45:59 +0200

    Update main.js

--- homeTasks3/Task#5/app/main.js
+++ homeTasks3/Task#5/app/main.js
@@ -1,10 +1,5 @@
 function circle() {
 
-    // out.innerHTML += bgC.value+' px;'; // delete after
-    // out.innerHTML += diametr.value+' px;'; // delete after
-    // out.innerHTML += circle; // delete after
-
-    // bgC.style.borderColor = bgC.value;
     circleDiv.style.width = diametr.value + "px";
     circleDiv.style.height = diametr.value + "px";
     circleDiv.style.borderRadius = "50%";
