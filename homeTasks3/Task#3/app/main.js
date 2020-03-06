a0be841bc30ca7904fd6d6c7b1ba181dc7d283a9
gmaxym <gorbenkomaxym@gmail.com>
2020-03-06 11:15:00 +0200

    Update main.js

--- homeTasks3/Task#3/app/main.js
+++ homeTasks3/Task#3/app/main.js
@@ -1,3 +1,5 @@
+// спросить как обернуть в функцию
+
 let str = "Lets all go on holiday somewhere very cold";
 let arrStr = str.split(' '); //[0]I(0.1), [1]know(1.4), [2]JS(2.2),
  let minWord = arrStr[0].length; //1
